e3b7bbfb33f62a26b07847aca4072f3a69f3dc99
gummireddysandeepreddy <gummireddysandeepreddy@gmail.com>
2024-08-15 23:21:50 +0530

    Skills Section Added

---
+++ src/app/skills.tsx
@@ -0,0 +1,27 @@
+import { Parallax, ParallaxProvider, useParallax } from 'react-scroll-parallax';
+import { Skills as s } from '@/lib/skills';
+export default function Skills() {
+    let a : [number, number] = [-10,10];
+    const toggleA = (): [number, number] => {
+        a = [-a[0], -a[1]];
+        return a;
+    }
+    return (
+        <div className='h-auto w-full overflow-hidden' id='skills'>
+                <div className='font-bold text-2xl'>Skills</div>
+                {s.map((skill) => (
+                    <div key={skill.name} className='overflow-hidden my-5'>
+                        {/* <div className='font-bold text-xl'>{skill.name}</div> */}
+                        <Parallax speed={-1} translateX={toggleA()} className='overflow-hidden'>
+                            <p className='text-2xl font-mono overflow-hidden'>
+                            {skill.list.map((s) => (
+                                <p key={s} className='inline-block'>{s}/</p>
+                            ))}
+                            </p>
+                        </Parallax>
+                    </div>
+                ))}
+        </div>
+    );
+}
+
